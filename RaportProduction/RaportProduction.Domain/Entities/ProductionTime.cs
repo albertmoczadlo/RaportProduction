f6683637f9f4061d80559e2b4857629e3e062204
Albert <albertmoczadlo@gmail.com>
2023-04-02 11:21:16 +0200

    Added new entities.

---
+++ RaportProduction/RaportProduction.Domain/Entities/ProductionTime.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaportProduction.Domain.Entities;
+
+public class ProductionTime
+{
+    public int Id { get; set; }
+    public DayOfWeek Day { get; set; }
+    public DateTime StartProductionTime { get; set; }
+    public DateTime? EndProductionTime { get; set; }
+    public int RaportId { get; set; }
+
+    public Raport Raport { get; set; }
+}
--- RaportProduction/RaportProduction.Domain/Entities/Raport.cs
+++ RaportProduction/RaportProduction.Domain/Entities/Raport.cs
@@ -1,6 +1,20 @@
-﻿namespace RaportProduction.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace RaportProduction.Domain.Entities;
+
+public class Raport
 {
-    public class Raport
-    {
-    }
+
+    public int Id { get; set; }
+    public int Quantity { get; set; }
+    public string Comments { get; set; }
+    public string UserId { get; set; }
+
+
+    public ApplicationUser User { get; set; }
+    public Product Product { get; set; }
+    public Container Containers { get; set; }
+    public ProductionTime ProductionTime { get; set; }
+
 }
