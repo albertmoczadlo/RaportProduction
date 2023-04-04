dc231f02bd9533a97132db53497ccb3fe35a982b
Albert <albertmoczadlo@gmail.com>
2023-04-04 03:47:08 +0200

    Fixed problem entities configuration

---
+++ RaportProduction/RaportProduction.Infrastructure/Persistance/Configurations/FileConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using File = RaportProduction.Domain.Entities.File;
+
+namespace RaportProduction.Infrastructure.Persistence.Configurations;
+
+class FileConfiguration : IEntityTypeConfiguration<File>
+{
+    public void Configure(EntityTypeBuilder<File> builder)
+    {
+        builder.ToTable("Files");
+
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasMaxLength(300);
+
+        builder.Property(x => x.Description)
+            .IsRequired();
+    }
+}
