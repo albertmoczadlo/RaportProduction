14781dd78c85a93267a9e5796da71c68958a3ab1
Albert <albertmoczadlo@gmail.com>
2023-04-12 08:56:05 +0200

    Added LoggingBehaviour, PerformanceBehaviour.

---
+++ RaportProduction/RaportProduction.Application/Common/Behaviours/LoggingBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace RaportProduction.Application.Common.Behaviours;
+
+public class LoggingBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    private readonly ILogger _logger;
+
+    public LoggingBehaviour(ILogger<TRequest> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, 
+        RequestHandlerDelegate<TResponse> next)
+    {
+        var requestName = typeof(TRequest).Name;
+
+        _logger.LogInformation($"Handling {requestName}");
+
+        _logger.LogInformation("RaportProduction: {@Name} {@Request}",
+            requestName, request);
+
+        var response = await next();
+
+        _logger.LogInformation($"Handled {typeof(TResponse).Name}");
+
+        return response;
+
+    }
+}
--- RaportProduction/RaportProduction.Application/DependencyInjection.cs
+++ RaportProduction/RaportProduction.Application/DependencyInjection.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using MediatR;
 using System.Reflection;
+using RaportProduction.Application.Common.Behaviours;
 
 namespace RaportProduction.Application;
 
@@ -9,6 +10,10 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(Assembly.GetExecutingAssembly());
+
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehaviour<,>));
+
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
 
         return services;
     }
