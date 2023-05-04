30c321c5c4bdef52de0548c4a2b0f1d8e63ce359
Albert <albertmoczadlo@gmail.com>
2023-05-04 12:37:22 +0200

    Added method handle to validationBehavior

--- RaportProduction/RaportProduction.Application/Common/Behaviours/ValidationBehaviour.cs
+++ RaportProduction/RaportProduction.Application/Common/Behaviours/ValidationBehaviour.cs
@@ -1,13 +1,16 @@
-﻿using MediatR;
+﻿using FluentValidation;
+using MediatR;
 
 namespace RaportProduction.Application.Common.Behaviours;
 
 public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
     where TRequest : IRequest<TResponse>
 {
+    private readonly IEnumerable<IValidator<TRequest>> _validator;
 
-    public ValidationBehaviour()
+    public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validator)
     {
+        _validator = validator;
     }
 
     public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken,
