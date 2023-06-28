5b061b7887bc6fd36500a2703b76c3b4c2b7678a
Albert <albertmoczadlo@gmail.com>
2023-06-28 10:23:16 +0200

    Added data annotation

--- RaportProduction/RaportProduction.Application/Contacts/Commands/SendContactEmail/SendContactEmailCommandValidator.cs
+++ RaportProduction/RaportProduction.Application/Contacts/Commands/SendContactEmail/SendContactEmailCommandValidator.cs
@@ -4,9 +4,9 @@ namespace RaportProduction.Application.Contacts.Commands.SendContactEmail;
 
 public class SendContactEmailCommandValidator:AbstractValidator<SendContactEmailCommand>
 {
-	public SendContactEmailCommandValidator()
-	{
-		RuleFor(x => x.Name)
-			.NotEmpty().WithMessage("Name and surname field is mandatory");
-	}
+	//public SendContactEmailCommandValidator()
+	//{
+	//	RuleFor(x => x.Name)
+	//		.NotEmpty().WithMessage("Name and surname field is mandatory");
+	//}
 }
